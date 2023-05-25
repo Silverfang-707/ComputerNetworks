2285995bd2b91c973245af156efba9af968ddab1
Ramsundar <112865253+Silverfang707@users.noreply.github.com>
2023-05-25 15:17:39 +0530

    Update hammingcode.c

--- hammingcode.c
+++ hammingcode.c
@@ -26,7 +26,7 @@ void errorDetection(){
 	c1=rec[6]^rec[4]^rec[2]^rec[0];
 	c2=rec[5]^rec[4]^rec[1]^rec[0];
 	c3=rec[3]^rec[2]^rec[1]^rec[0];
-	c=c3*4+c2*2+c1 ;
+	c=c3*4+c2*2+c1;
 	if(c==0) {
 		printf("\nCongratulations there is no error! \n");
 	} 
